d0df8375a099955fd06a414ad0549b7d40480272
PlucinskiP <plucinski@gmx.com>
2020-11-06 15:20:37 +0100

    test

--- scripts/run.py
+++ scripts/run.py
@@ -16,18 +16,6 @@ for labels in pr.labels:
     if labels.name == 'automated_pr':
         label_check = True
 
-token = "token "+os.environ.get('GITHUB_TOKEN')
-url_approve = "https://api.github.com/repos/PlucinskiP/hello-github-actions/pulls/"+str(pull_no)+"/reviews"
-url_merge = "https://api.github.com/repos/PlucinskiP/hello-github-actions/pulls/"+str(pull_no)+"/merge"
 if label_check:
-
-    headers_approve = {
-        'Authorization': token,
-    }
-    data = '{"event": "APPROVE"}'
-    approve_res = requests.post(url_approve, headers=headers_approve, data=data)
-
-    headers_merge = {
-        'Authorization': token,
-    }
-    merge_res = requests.put(url_merge, headers=headers_merge)
+    approve_pr = commit.get_pulls()[0].number.reviews( event="APPROVE" )
+    merge_pr = commit.get_pulls()[0].merge
